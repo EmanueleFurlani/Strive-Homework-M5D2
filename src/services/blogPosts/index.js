e3247f8fbb593ff0ef81528a0ec114ab83f5d12c
emanuele <emanuelefurlani.go@gmail.com>
2021-09-22 16:48:37 +0200

    Update index.js

--- src/services/blogPosts/index.js
+++ src/services/blogPosts/index.js
@@ -11,9 +11,7 @@ import { validationResult } from "express-validator";
 import { blogPostValidation, blogPostCommentValidation } from "./validation.js";
 import multer from "multer";
 import {pipeline} from "stream";
-import {
-  getBlogPostPDFReadableStream,
-} from "../../lib/tools.js" 
+import getBlogPostPDFReadableStream from "../../lib/tools.js" 
 
 const blogPostsRouter = express.Router();
 
